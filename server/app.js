c6d8f5e18ad530919a3a20574a922802f85705e8
Mario Aguilar <marioaguilar@marios-mbp.home>
2019-01-06 16:40:11 -0600

    v.0.4.0

--- server/app.js
+++ server/app.js
@@ -25,11 +25,11 @@ app.use((req, res, next) => {
 })
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 /**
